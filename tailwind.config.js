7c54e28ba63e641bd62bd2fe4e30af5a2ee2f2fb
Lukas Dias <diaslukas19@gmail.com>
2021-02-28 23:48:08 -0300

    (Feat):Insert TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
